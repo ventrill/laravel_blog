1df32e14b8ac3daf0d60629f358fe285e50de2de
Alex Vasilenko <a.vasilenko@smart-it.by>
2021-04-07 08:57:48 +0300

    save

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,6 +85,14 @@
                 </div>
 
                 <div class="links">
+                    @foreach ($links as $link)
+                        <div>
+                        <a href="{{ $link->url }}">{{ $link->title }}</a>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = \App\Link::all();
+    return view('welcome')->with('links', $links);
+});
+Route::get('/submit', function () {
+    return view('link.submit');
 });
 
-Auth::routes();
+\Illuminate\Support\Facades\Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
